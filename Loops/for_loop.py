ed711018abc894b98fd614d6bb12843960b5b652
Aashay Patil <118736994+theashay@users.noreply.github.com>
2025-02-28 16:02:39 +0530

    Add files via upload

---
+++ Loops/for_loop.py
@@ -0,0 +1,21 @@
+# for loop
+
+"""
+for no in range(10):
+    print(no)
+"""
+
+"""
+# print all even numbers from 1 to 100
+for no in range(2,101,2):
+    print(no)
+"""
+
+"""
+for ch in "Hello World":
+    print(ch5)
+"""
+
+for ch in "Hello World":
+    print(ch*5)
+
---
+++ Loops/while_loop.py
@@ -0,0 +1,6 @@
+no = 1
+while no<=10:
+    print(no, end=" ")
+    no += 1
+
+print("End")
